affa523523c1758c82d60e20bc9d95eeb9d7bb50
flsp <florian.lesp@gmail.com>
2025-06-03 08:12:00 +0200

    feat: create Navbar component and add it to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import Home from "./pages/HomePage";
 import Favorites from "./pages/FavoritesPage";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favoris' element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favoris' element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <Link to='/'>Movie Hub</Link>
+      </div>
+
+      <div className='navbar-links'>
+        <Link to='/' className='nav-link'>
+          Accueil
+        </Link>
+        <Link to='/favorites' className='nav-link'>
+          Favoris
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
