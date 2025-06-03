7110c5502f8b5c4b105ea5aa779a58af532cade7
flsp <florian.lesp@gmail.com>
2025-06-03 07:43:10 +0200

    feat: Create FavortiePage

---
+++ frontend/src/pages/FavoritePage.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+  return (
+    <div className='favorites-empty'>
+      <h2>Pas de favoris pour le moment !</h2>
+      <p>Ajoutez des films dans vos favoris et ils apparaîtront ici.</p>
+    </div>
+  );
+}
+
+export default Favorite;
